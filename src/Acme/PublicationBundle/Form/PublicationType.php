fe0d0d3d12352dd954ddc75e16f5be457c17fd4b
Natalia Stanko <anithaly@gmail.com>
2014-08-19 05:35:05 +0200

    CRUD publication

---
+++ src/Acme/PublicationBundle/Form/PublicationType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acme\PublicationBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PublicationType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\PublicationBundle\Entity\Publication'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acme_publicationbundle_publication';
+    }
+}
