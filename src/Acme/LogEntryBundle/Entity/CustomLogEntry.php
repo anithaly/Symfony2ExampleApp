1bfda634d8126602f6aeedf3494d17ac8df5e76b
Natalia Stanko <anithaly@gmail.com>
2014-12-13 23:08:36 +0100

    Add Custom log entry table

--- src/Acme/LogEntryBundle/Entity/CustomLogEntry.php
+++ src/Acme/LogEntryBundle/Entity/CustomLogEntry.php
@@ -17,9 +17,21 @@ class CustomLogEntry extends LogEntry
 {
 
     /**
+     * Serialized array of old data
      * @ORM\Column(type="array", name="before_data", nullable=true)
      */
     protected $before_data;
+
+    /**
+     * @ORM\Column(type="integer", name="user_id")
+     */
+    protected $user_id;
+
+    /**
+     * Is checked as read
+     * @ORM\Column(type="boolean", name="is_checked")
+     */
+    protected $is_checked = false;
 
     public function setBeforeData($before_data)
     {
@@ -31,6 +43,30 @@ class CustomLogEntry extends LogEntry
     public function getBeforeData()
     {
         return $this->before_data;
+    }
+
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    public function setIsChecked($is_checked)
+    {
+        $this->is_checked = $is_checked;
+
+        return $this;
+    }
+
+    public function getIsChecked()
+    {
+        return $this->is_checked;
     }
 
 }
